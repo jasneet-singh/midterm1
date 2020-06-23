6c50c279295cbe6f7ae6cce8052ec29089a4bc79
ProfPaulB <paul.bonenfant@sheridancollege.ca>
2020-06-23 09:55:09 -0400

    Third Commit

--- src/midterm/RegistrationPlate.java
+++ src/midterm/RegistrationPlate.java
@@ -12,8 +12,8 @@ package midterm;
  */
 public class RegistrationPlate {
 
-    public String plateNumber;
-    public String plateType;
+    private String plateNumber;
+    private String plateType;
     private String[] registrationTypes = {"Commercial", "Non-Commercial", "Personalized"};
 
     /**
